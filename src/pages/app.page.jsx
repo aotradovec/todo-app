4616d86e598452f155bc56b5f536acec7288961b
aotradovec <adam.otradovec@seznam.cz>
2021-02-10 16:26:25 +0100

    About page

--- src/pages/app.page.jsx
+++ src/pages/app.page.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import styles from './app.module.css';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { Home } from './home.page';
+import { About } from './about.page';
 
 export function App() {
   return (
     <div id={styles.app}>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
           <Redirect to="/" />
         </Switch>
       </BrowserRouter>
